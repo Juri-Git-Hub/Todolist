ae9aeb78d2dcc199ab276f8a4b2238069acbe10e
Juri-Git-Hub <137108726+Juri-Git-Hub@users.noreply.github.com>
2023-10-08 17:23:01 +0200

    first version of server
    +work.ejs created

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs")
+})
+
+app.get("/work", (req,res) => {
+    res.render("work.ejs")
+})
+
+//app.post("/addtask", (req,res) => {
+//    res.render("index.ejs")
+//})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
